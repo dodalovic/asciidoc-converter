eca24bd91c8bc9abacca9db7197ee39b4d57fae3
Dusan Odalovic <dusan.odalovic@whitewall.com>
2017-10-08 21:57:44 +0200

    NOT WORKING: Support configurable output directory for generated file

--- src/main/kotlin/rs/dodalovic/asciidoc/Converter.kt
+++ src/main/kotlin/rs/dodalovic/asciidoc/Converter.kt
@@ -1,12 +1,19 @@
 package rs.dodalovic.asciidoc
 
 import org.asciidoctor.Asciidoctor.Factory.create
+import org.asciidoctor.Options
 import org.asciidoctor.OptionsBuilder.options
 import org.asciidoctor.SafeMode
 import java.io.File
 
 fun main(args: Array<String>) = asciidocFiles().forEach { create().convertFile(it, configOptions()) }
 
-private fun configOptions() = options().safe(SafeMode.SAFE).backend(System.getProperty("backend") ?: "html")
+private fun configOptions(): Options {
+    val options = options()
+            .safe(SafeMode.SAFE)
+            .backend(System.getProperty("backend") ?: "html")
+    System.getProperty("outputDir")?.let { options.baseDir(File(it)) }
+    return options.get()
+}
 
-private fun asciidocFiles() = System.getProperty("asciidocFiles").split(",").map { File(it) }
+private fun asciidocFiles() = System.getProperty("files").split(",").map { File(it) }
