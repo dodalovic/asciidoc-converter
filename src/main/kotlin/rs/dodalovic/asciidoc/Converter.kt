86f253cd08fbf7eb21f16dff4a0a8c66fbc3c331
Dusan Odalovic <dusan.odalovic@whitewall.com>
2017-10-09 22:07:35 +0200

    Rename var

--- src/main/kotlin/rs/dodalovic/asciidoc/Converter.kt
+++ src/main/kotlin/rs/dodalovic/asciidoc/Converter.kt
@@ -9,11 +9,11 @@ import java.io.File
 fun main(args: Array<String>) = adocFiles.forEach { create().convertFile(it, configOptions()) }
 
 private fun configOptions(): Options {
-    val options = options()
+    val optionsBuilder = options()
             .safe(SafeMode.UNSAFE)
             .backend(System.getProperty("backend") ?: "html")
-    System.getProperty("outputDir")?.let { options.mkDirs(true).toDir(File(it)) }
-    return options.get()
+    System.getProperty("outputDir")?.let { optionsBuilder.mkDirs(true).toDir(File(it)) }
+    return optionsBuilder.get()
 }
 
 private val adocFiles: List<File> = System.getProperty("files").split(",").map { File(it) }
