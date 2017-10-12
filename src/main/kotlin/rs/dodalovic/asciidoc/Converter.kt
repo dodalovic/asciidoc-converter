2dceae61022e4ae3f1b71344220a85697f31f8fc
Dusan Odalovic <dusan.odalovic@whitewall.com>
2017-10-12 09:50:56 +0200

    Update implementation with FILE/DIR processing support

--- src/main/kotlin/rs/dodalovic/asciidoc/Converter.kt
+++ src/main/kotlin/rs/dodalovic/asciidoc/Converter.kt
@@ -1,21 +1,65 @@
 package rs.dodalovic.asciidoc
 
+import org.asciidoctor.AsciiDocDirectoryWalker
 import org.asciidoctor.Asciidoctor.Factory.create
 import org.asciidoctor.Options
 import org.asciidoctor.OptionsBuilder.options
 import org.asciidoctor.SafeMode
 import java.io.File
 
-fun main(args: Array<String>) = adocFiles.forEach {
-    create().convertFile(it, configOptions())
+fun main(args: Array<String>) {
+    RequestProcessorFactory.getProcessor().process()
 }
 
 private fun configOptions(): Options {
     val optionsBuilder = options()
             .safe(SafeMode.UNSAFE)
+            .mkDirs(true)
             .backend(System.getProperty("backend") ?: "html")
-    System.getProperty("outputDir")?.let { optionsBuilder.mkDirs(true).toDir(File(it)) }
+    System.getProperty("outputDir")?.let { optionsBuilder.toDir(File(it)) }
     return optionsBuilder.get()
 }
 
-private val adocFiles: List<File> = System.getProperty("files").split(",").map { File(it) }
+object RequestProcessorFactory {
+    enum class ApplicationMode {
+        FILE, DIR
+    }
+
+    private val mode = detectMode()
+
+    fun getProcessor(): RequestProcessor {
+        return when (mode) {
+            ApplicationMode.FILE -> FileProcessor()
+            ApplicationMode.DIR -> DirProcessor()
+            else -> FileProcessor()
+        }
+    }
+
+    private fun detectMode(): ApplicationMode {
+        return try {
+            ApplicationMode.valueOf(System.getProperty("mode"))
+        } catch (ex: IllegalArgumentException) {
+            return ApplicationMode.FILE
+        }
+    }
+}
+
+interface RequestProcessor {
+    fun process()
+}
+
+class FileProcessor : RequestProcessor {
+    override fun process() {
+        val adocFiles = System.getProperty("files").split(",").map { File(it) }
+        adocFiles.forEach {
+            create().convertFile(it, configOptions())
+        }
+    }
+}
+
+class DirProcessor : RequestProcessor {
+    override fun process() {
+        System.getProperty("dirs").split(",")
+                .forEach { create().convertDirectory(AsciiDocDirectoryWalker(it), configOptions()) }
+    }
+}
